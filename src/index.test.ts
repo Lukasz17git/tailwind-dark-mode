b3416a620b9f21429229c89c2cbe7f760aadb9e3
Lukasz17git <lucas.m2295@gmail.com>
2023-07-30 23:45:30 +0200

    added dummy test file

---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest'
+
+describe('addDarkModeUtilities', () => {
+   it('should be able to add new utilities with dark mode', () => {
+      expect(true).toBe(true)
+   })
+})
